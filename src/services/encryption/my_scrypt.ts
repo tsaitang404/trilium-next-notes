0029ad94e826e66032e516c899d57bd8e2af45ac
Chesspro13 <chesspro13@gmail.com>
2024-06-12 12:27:05 -0700

    Working on decryption

--- src/services/encryption/my_scrypt.ts
+++ src/services/encryption/my_scrypt.ts
@@ -6,55 +6,74 @@ import utils = require("../utils");
 import sql = require("../sql");
 
 function getVerificationHash(password: crypto.BinaryLike) {
-  const salt = optionService.getOption("passwordVerificationSalt");
+    const salt = optionService.getOption("passwordVerificationSalt");
 
-  return getScryptHash(password, salt);
+    return getScryptHash(password, salt);
 }
 
 function getPasswordDerivedKey(password: crypto.BinaryLike) {
-  const salt = optionService.getOption("passwordDerivedKeySalt");
+    const salt = optionService.getOption("passwordDerivedKeySalt");
 
-  return getScryptHash(password, salt);
+    return getScryptHash(password, salt);
 }
 
-function getScryptHash(password: crypto.BinaryLike, salt: string | crypto.BinaryLike) {
-  const hashed = crypto.scryptSync(password, salt instanceof String ? utils.toBase64(salt.toString()) : salt, 32, {
-    N: 16384,
-    r: 8,
-    p: 1,
-  });
+function getScryptHash(
+    password: crypto.BinaryLike,
+    salt: string | crypto.BinaryLike
+) {
+    const hashed = crypto.scryptSync(
+        password,
+        salt instanceof String ? utils.toBase64(salt.toString()) : salt,
+        32,
+        {
+            N: 16384,
+            r: 8,
+            p: 1,
+        }
+    );
 
-  return hashed;
+    return hashed;
 }
 
-function getSubjectIdentifierVerificationHash(password: crypto.BinaryLike, salt: crypto.BinaryLike) {
-  // const salt = optionService.getOption('subjectIdentifierVerificationSalt');
+function getSubjectIdentifierVerificationHash(
+    guessedUserId: string | crypto.BinaryLike
+) {
+    // const salt = optionService.getOption('subjectIdentifierVerificationSalt');
 
-  return getScryptHash(password, salt);
+    const salt = sql.getValue("SELECT userIDVerificationSalt FROM user_data");
+    if (salt === undefined || salt === null) {
+        console.log("User salt undefined!");
+        return undefined;
+    }
+    console.log(guessedUserId, salt);
+    return getScryptHash(guessedUserId, utils.toBase64(salt.toString()));
 }
 
 function getSubjectIdentifierDerivedKey(subjectIdentifer: crypto.BinaryLike) {
-  // const salt = optionService.getOption("subjectIdentifierDerivedKeySalt");
+    // const salt = optionService.getOption("subjectIdentifierDerivedKeySalt");
 
-  const salt = sql.getValue("SELECT userIDVerificationSalt FROM user_data");
-  if (salt === undefined || salt === null) return undefined;
+    const salt = sql.getValue("SELECT userIDVerificationSalt FROM user_data");
+    if (salt === undefined || salt === null) return undefined;
 
-  return getScryptHash(subjectIdentifer, utils.toBase64(salt.toString()));
+    return getScryptHash(subjectIdentifer, utils.toBase64(salt.toString()));
 }
 
-function createSubjectIdentifierDerivedKey(subjectIdentifer: string | crypto.BinaryLike, verificationSalt: string | crypto.BinaryLike) {
-  // const salt = optionService.getOption("subjectIdentifierDerivedKeySalt");
+function createSubjectIdentifierDerivedKey(
+    subjectIdentifer: string | crypto.BinaryLike,
+    verificationSalt: string | crypto.BinaryLike
+) {
+    // const salt = optionService.getOption("subjectIdentifierDerivedKeySalt");
 
-  //   const salt = sql.getValue("SELECT userIDVerificationSalt FROM user_data");
-  //   if (salt === undefined || salt === null) return undefined;
+    //   const salt = sql.getValue("SELECT userIDVerificationSalt FROM user_data");
+    //   if (salt === undefined || salt === null) return undefined;
 
-  return getScryptHash(subjectIdentifer, verificationSalt);
+    return getScryptHash(subjectIdentifer, verificationSalt);
 }
 
 export = {
-  getVerificationHash,
-  getPasswordDerivedKey,
-  getSubjectIdentifierVerificationHash,
-  getSubjectIdentifierDerivedKey,
-  createSubjectIdentifierDerivedKey,
+    getVerificationHash,
+    getPasswordDerivedKey,
+    getSubjectIdentifierVerificationHash,
+    getSubjectIdentifierDerivedKey,
+    createSubjectIdentifierDerivedKey,
 };
--- src/services/encryption/open_id_encryption.ts
+++ src/services/encryption/open_id_encryption.ts
@@ -6,96 +6,148 @@ import sql = require("../sql");
 import sqlInit = require("../sql_init");
 
 function saveSubjectIdentifier(subjectIdentifier: string) {
-  const verificationSalt = utils.randomSecureToken(32);
-  const derivedKeySalt = utils.randomSecureToken(32);
-  const verificationHash = utils.toBase64(myScryptService.getSubjectIdentifierVerificationHash(subjectIdentifier, verificationSalt));
-  const userIDEncryptedDataKey = setDataKey(subjectIdentifier, utils.randomSecureToken(16), derivedKeySalt);
+    const verificationSalt = utils.randomSecureToken(32);
+    const derivedKeySalt = utils.randomSecureToken(32);
 
-  if (userIDEncryptedDataKey === undefined || userIDEncryptedDataKey === null) console.log("USERID ENCRYPTED DATA KEY NULL");
+    sql.replace("user_data", {
+        userIDVerificationSalt: verificationSalt,
+        userIDDerivedKeySalt: derivedKeySalt,
+    });
 
-  sql.replace("user_data", {
-    userIDVerificationHash: verificationHash,
-    userIDVerificationSalt: verificationSalt,
-    userIDDerivedKey: derivedKeySalt,
-    userIDEcnryptedDataKey: userIDEncryptedDataKey,
-    isSetup: "true",
-  });
+    const verificationHash =
+        myScryptService.getSubjectIdentifierVerificationHash(subjectIdentifier);
 
-  return {
-    success: true,
-  };
+    const userIDEncryptedDataKey = setDataKey(
+        subjectIdentifier,
+        utils.randomSecureToken(16)
+    );
+
+    if (userIDEncryptedDataKey === undefined || userIDEncryptedDataKey === null)
+        console.log("USERID ENCRYPTED DATA KEY NULL");
+
+    console.log("Saving...");
+    const data = {
+        userIDVerificationHash: verificationHash,
+        userIDVerificationSalt: verificationSalt,
+        userIDDerivedKey: derivedKeySalt,
+        userIDEcnryptedDataKey: userIDEncryptedDataKey,
+        isSetup: "true",
+    };
+
+    console.log(data);
+    sql.replace("user_data", data);
+    console.log("Saved userID");
+    return {
+        success: true,
+    };
 }
 
 function isSubjectIdentifierSaved() {
-  const value = sql.getValue("SELECT userIDEcnryptedDataKey FROM user_data");
-  if (value === undefined || value === null || value === "") return false;
-  return true;
+    const value = sql.getValue("SELECT userIDEcnryptedDataKey FROM user_data");
+    if (value === undefined || value === null || value === "") return false;
+    return true;
 }
 
 function isUserSaved() {
-  return sql.getValue("SELECT isSetup FROM user_data") == "true" ? true : false;
+    return sql.getValue("SELECT isSetup FROM user_data") == "true"
+        ? true
+        : false;
 }
 
 function verifyOpenIDSubjectIdentifier(subjectIdentifier: string) {
-  // Check to see if table exists
-  if (!sqlInit.isDbInitialized()) return undefined;
+    console.log("Verifying UserID");
+    // Check to see if table exists
+    if (!sqlInit.isDbInitialized()) return undefined;
 
-  if (!isUserSaved()) {
-    console.log("DATABASE NOT SETUP");
-    return undefined;
-  }
+    if (!isUserSaved()) {
+        console.log("DATABASE NOT SETUP");
+        return undefined;
+    }
 
-  const salt = sql.getValue("SELECT userIDVerificationSalt FROM user_data");
-  console.log(salt);
-  if (salt == undefined) return undefined;
+    const salt = sql.getValue("SELECT userIDVerificationSalt FROM user_data");
+    console.log("Salt: " + salt);
+    if (salt == undefined) {
+        console.log("Salt undefined");
+        return undefined;
+    }
 
-  const givenSubjectIdentifierHash = utils.toBase64(myScryptService.getSubjectIdentifierVerificationHash(subjectIdentifier, utils.toBase64(salt.toString())));
+    const givenSubjectIdentifierHash =
+        myScryptService.getSubjectIdentifierVerificationHash(
+            utils.toBase64(subjectIdentifier)
+        );
 
-  const hash = sql.getValue("SELECT userIDVerificationHash FROM user_data");
-  if (hash === undefined) return undefined;
+    console.log(
+        "Passed userid Hash: " + givenSubjectIdentifierHash?.toString("base64")
+    );
 
-  const dbSubjectIdentifierHash = hash;
+    const hash = sql.getValue("SELECT userIDVerificationHash FROM user_data");
+    if (hash === undefined) {
+        console.log("verification hash undefined");
+        return undefined;
+    }
+    console.log("Saved userid Hash: " + hash);
 
-  if (!hash) return false;
+    const dbSubjectIdentifierHash = hash;
 
-  console.log("Matches: " + givenSubjectIdentifierHash === hash);
-  console.log(givenSubjectIdentifierHash);
-  console.log(hash);
-  return givenSubjectIdentifierHash === hash;
+    // if (!hash) return false;
+
+    console.log(givenSubjectIdentifierHash, hash);
+    console.log("Matches: " + givenSubjectIdentifierHash === hash);
+    console.log(givenSubjectIdentifierHash);
+    console.log("Hash 2: " + hash);
+    return givenSubjectIdentifierHash === hash;
 }
 
-function setDataKey(subjectIdentifier: string, plainTextDataKey: string | Buffer, salt: string) {
-  // Need to figure out which salt. Don't have the brain power for encryption today.
-  const subjectIdentifierDerivedKey = myScryptService.createSubjectIdentifierDerivedKey(subjectIdentifier, salt);
+function setDataKey(
+    subjectIdentifier: string,
+    plainTextDataKey: string | Buffer
+) {
+    // Need to figure out which salt. Don't have the brain power for encryption today.
+    const subjectIdentifierDerivedKey =
+        myScryptService.getSubjectIdentifierDerivedKey(subjectIdentifier);
 
-  if (subjectIdentifierDerivedKey === undefined) {
-    console.log("SOMETHING WENT WRONG SAVING USER ID DERIVED KEY");
-    return undefined;
-  }
-  const newEncryptedDataKey = dataEncryptionService.encrypt(subjectIdentifierDerivedKey, plainTextDataKey);
+    if (subjectIdentifierDerivedKey === undefined) {
+        console.log("SOMETHING WENT WRONG SAVING USER ID DERIVED KEY");
+        return undefined;
+    }
+    const newEncryptedDataKey = dataEncryptionService.encrypt(
+        subjectIdentifierDerivedKey,
+        plainTextDataKey
+    );
 
-  // optionService.setOption('subjectIdentifierEncryptedDataKey', newEncryptedDataKey);
-  return newEncryptedDataKey;
+    // optionService.setOption('subjectIdentifierEncryptedDataKey', newEncryptedDataKey);
+    return newEncryptedDataKey;
 }
 
 function getDataKey(subjectIdentifier: string) {
-  const subjectIdentifierDerivedKey = myScryptService.getSubjectIdentifierDerivedKey(subjectIdentifier);
+    const subjectIdentifierDerivedKey =
+        myScryptService.getSubjectIdentifierDerivedKey(subjectIdentifier);
 
-  const encryptedDataKey = optionService.getOption("subjectIdentifierEncryptedDataKey");
+    const encryptedDataKey = sql.getValue(
+        "SELECT userIDEcnryptedDataKey FROM user_data"
+    );
 
-  if (subjectIdentifierDerivedKey === undefined) {
-    console.log("SOMETHING WENT WRONG SAVING USER ID DERIVED KEY");
-    return undefined;
-  }
-  const decryptedDataKey = dataEncryptionService.decrypt(subjectIdentifierDerivedKey, encryptedDataKey);
+    if (encryptedDataKey === undefined || encryptedDataKey === null) {
+        console.log("Encrypted data key empty!");
+        return undefined;
+    }
 
-  return decryptedDataKey;
+    if (subjectIdentifierDerivedKey === undefined) {
+        console.log("SOMETHING WENT WRONG SAVING USER ID DERIVED KEY");
+        return undefined;
+    }
+    const decryptedDataKey = dataEncryptionService.decrypt(
+        subjectIdentifierDerivedKey,
+        encryptedDataKey.toString()
+    );
+
+    return decryptedDataKey;
 }
 
 export = {
-  verifyOpenIDSubjectIdentifier,
-  getDataKey,
-  setDataKey,
-  saveSubjectIdentifier,
-  isSubjectIdentifierSaved,
+    verifyOpenIDSubjectIdentifier,
+    getDataKey,
+    setDataKey,
+    saveSubjectIdentifier,
+    isSubjectIdentifierSaved,
 };
